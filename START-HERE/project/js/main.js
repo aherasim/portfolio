220bebd69a27d0c50d09f1663a2b62fdb40bd382
alex herasimovich <alex herasimovich>
2016-12-13 01:27:03 -0500

    1

---
+++ START-HERE/project/js/main.js
@@ -0,0 +1,19 @@
+(function($){
+$(document).ready(function (){
+$(function(){
+
+$('#play').click(function(){$('#slider').cycle("resume"); return false;});
+
+$('#pause').click(function(){$('#slider').cycle("pause"); return false;});
+
+  $('#slider').cycle({
+ fx:     'scrollHorz',
+ next:   '#next',
+ prev:   '#prev',
+ pager:  '#pager',
+ timeout:   3000,
+ speed:  650,
+});
+});
+});
+})(window.jQuery);
